c696b03f129e34b124e17ba1ba0e235a11de7a4c
adiattara <136824969+adiattara@users.noreply.github.com>
2023-11-23 10:54:04 +0100

    error in url

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  'postgres://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE', // TODO: database connection string
+  'postgres://USER:PASSWORD@INTERNAL_HOST:PORT/node-postgres-sql', // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
