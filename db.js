0bb9ff061d8a08e078fa16290d35492f27c74dad
adiattara <136824969+adiattara@users.noreply.github.com>
2023-11-23 10:46:29 +0100

    add database service

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  'postgres://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE', // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
