2df9b62335dd3e57aedacfb28fd12f97f739b83a
adiattara <136824969+adiattara@users.noreply.github.com>
2023-11-23 11:09:41 +0100

    change db url

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  'postgres://USER:PASSWORD@INTERNAL_HOST:5432/node-postgres-sql', // TODO: database connection string
+    process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
